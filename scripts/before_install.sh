12212bf74f9b3fe6856f94c591d518c9bcfe62ee
freeLks <rinshouin666@gmail.com>
2024-09-23 22:57:11 +0900

    fix:add cd file

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,3 +2,8 @@
 echo "Stopping current Docker containers (if any)..."
 docker stop $(docker ps -q) || true
 docker rm $(docker ps -a -q) || true
+
+# Export variables
+export AWS_ACCOUNT_ID=724758113747
+export AWS_REGION=ap-northeast-1
+export REPOSITORY_NAME=backend
