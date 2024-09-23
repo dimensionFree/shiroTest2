cc24d8007ae251a17780efef3738a10643b14a9e
freeLks <rinshouin666@gmail.com>
2024-09-24 01:22:10 +0900

    fix:add cd file

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -40,13 +40,7 @@ echo "DEV_MAIL_PASSWORD: $DEV_MAIL_PASSWORD"
 # 运行 Docker 容器并传递环境变量
 echo "gonna runing container"
 
-CONTAINER_ID=$(docker run -d -p 80:80 \
-  -e DB_URL="$DB_URL" \
-  -e DB_USERNAME="$DB_USERNAME" \
-  -e DB_PASSWORD="$DB_PASSWORD" \
-  -e MAIL_USERNAME="$DEV_MAIL_PASSWORD" \
-  -e MAIL_PASSWORD="$DEV_MAIL_USERNAME" \
-  $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$REPOSITORY_NAME:latest)
+CONTAINER_ID=$(docker run -d -p 80:80 -e DB_URL="$DB_URL" -e DB_USERNAME="$DB_USERNAME" -e DB_PASSWORD="$DB_PASSWORD" -e MAIL_USERNAME="$DEV_MAIL_USERNAME" -e MAIL_PASSWORD="$DEV_MAIL_PASSWORD" $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$REPOSITORY_NAME:latest)
 
 # 检查是否成功启动容器
 if [ $? -ne 0 ]; then
