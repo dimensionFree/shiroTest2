17d388b311b252f1926d26953c345e65890f43cc
freeLks <rinshouin666@gmail.com>
2024-09-23 16:15:16 +0900

    fix:add cd file

--- ci/scripts/before_install.sh
+++ ci/scripts/before_install.sh
@@ -2,8 +2,5 @@
 echo "Stopping current Docker containers (if any)..."
 docker stop $(docker ps -q) || true
 docker rm $(docker ps -a -q) || true
-# 设置 AWS 账户 ID 和区域作为环境变量
-export AWS_REGION="ap-northeast-1"
-export AWS_ACCOUNT_ID="724758113747"
-export REPOSITORY_NAME="backend"
+
 
