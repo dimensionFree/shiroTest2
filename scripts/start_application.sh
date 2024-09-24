f54d69f2f35b9bef3b8a5b35299f2fb42dc43f94
freeLks <rinshouin666@gmail.com>
2024-09-24 17:39:00 +0900

    fix:add cd file

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -49,7 +49,7 @@ echo "Docker Compose Installing..."
 COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')
 
 # 下载并安装 Docker Compose
-sudo curl -L "https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/${curl -s https://api.github.com/repos/docker/compose/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")'}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 sleep 20
 
