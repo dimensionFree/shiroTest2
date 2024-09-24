b104170fba31c4a67956c99e130b67764eeb1258
freeLks <rinshouin666@gmail.com>
2024-09-24 21:43:24 +0900

    fix:add cd file

--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -3,7 +3,7 @@ echo "Validating the service..."
 
 # 检查应用是否在 localhost:80 上运行
 echo "Checking if the application is running on localhost:80..."
-until curl -f http://localhost:80; do
+until curl http://localhost:80; do
   echo "Application is not yet available. Waiting..."
   sleep 5
 done
